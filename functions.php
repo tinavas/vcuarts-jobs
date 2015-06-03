1e97c9f302951a49f9c8193a568dba927aa2702e
Dan <dstrogiy@gmail.com>
2015-06-03 11:20:18 -0400

    we don't need translation

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
+
 <?php
 /*
 Author: Eddie Machado
@@ -26,8 +27,6 @@ function bones_ahoy() {
   //Allow editor style.
   add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' );
 
-  // let's get language support going, if you need it
-  load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
 
   // USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
   require_once( 'library/custom-post-type.php' );
