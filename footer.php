f8f2280348f44a1fe88634857a89a07cc877bda3
Dan <dstrogiy@gmail.com>
2015-06-25 11:42:29 -0400

    footer finesse

--- footer.php
+++ footer.php
@@ -21,8 +21,7 @@
 						)); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-          <p class="last-modified">Last modified: <?php the_modified_time('F j, Y'); ?></p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Last modified on <?php the_modified_time('F j, Y'); ?>.</p>
 				</div>
 
 			</footer>
