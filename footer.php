a6241e4cfe04cee61e620a5473467659b610dfef
Dan <dstrogiy@gmail.com>
2015-06-25 11:53:28 -0400

    vcuarts logo in foter

--- footer.php
+++ footer.php
@@ -2,26 +2,14 @@
 </div>
 
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-
 				<div class="inner-footer">
-
-					<nav role="navigation">
-						<?php wp_nav_menu(array(
-    					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-    					'container_class' => 'footer-links',         // class of container (should you choose to use it)
-    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-    					'menu_class' => 'nav footer-nav',            // adding custom nav class
-    					'theme_location' => 'footer-links',             // where it's located in the theme
-    					'before' => '',                                 // before the menu
-    					'after' => '',                                  // after the menu
-    					'link_before' => '',                            // before each link
-    					'link_after' => '',                             // after each link
-    					'depth' => 0,                                   // limit the depth of the nav
-    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-						)); ?>
-					</nav>
-
 					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Last modified on <?php the_modified_time('F j, Y'); ?>.</p>
+          <div class="footer-logo">
+            <a href="<?php echo site_url(); ?>">
+              <img class="monotone" src="<?php echo get_template_directory_uri(); ?>/library/images/svg/vcuarts.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/library/images/svg/fallbacks/vcuarts-dk.jpg'">
+              <img class="color" src="<?php echo get_template_directory_uri(); ?>/library/images/svg/vcuarts-hover.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/library/images/svg/fallbacks/vcuarts-dk-hover.jpg'">
+            </a>
+          </div>
 				</div>
 
 			</footer>
