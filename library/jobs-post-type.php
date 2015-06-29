ec082f1d0f039e47a2e5b8eeebff35b0d1a3e771
Dan <dstrogiy@gmail.com>
2015-06-29 11:26:15 -0400

    slug to job instead of jobs

--- library/jobs-post-type.php
+++ library/jobs-post-type.php
@@ -33,7 +33,7 @@ function custom_post_type() {
 			'query_var' => true,
 			'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */ 
 			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/custom-post-icon.png', /* the icon for the custom post type menu */
-			'rewrite'	=> array( 'slug' => 'jobs', 'with_front' => false ), /* you can specify its url slug */
+			'rewrite'	=> array( 'slug' => 'job', 'with_front' => false ), /* you can specify its url slug */
 			'has_archive' => false, /* you can rename the slug here */
 			'capability_type' => 'post',
 			'hierarchical' => false,
