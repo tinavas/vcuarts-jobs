2839990a529185fd5785e3ba643e6ffd97da050f
Dan <dstrogiy@gmail.com>
2015-06-29 11:08:18 -0400

    archive template init

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 				<div class="inner-footer">
 					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Last modified on <?php the_modified_time('F j, Y'); ?>.</p>
           <div class="footer-logo">
-            <a href="<?php echo site_url(); ?>">
+            <a href="http://arts.vcu.edu">
               <img class="monotone" src="<?php echo get_template_directory_uri(); ?>/library/images/svg/vcuarts.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/library/images/svg/fallbacks/vcuarts-dk.jpg'">
               <img class="color" src="<?php echo get_template_directory_uri(); ?>/library/images/svg/vcuarts-hover.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/library/images/svg/fallbacks/vcuarts-dk-hover.jpg'">
             </a>
