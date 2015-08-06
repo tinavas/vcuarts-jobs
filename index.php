52f35cfe673fceda8a3c943b0d4bd1231463368f
Cody W <cody@codywhitby.net>
2015-08-06 14:07:48 -0400

    dont need a custom post type actually, homepage should be posts page. do not push yet, jobs need to be moved to posts on live site

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
 <?php get_header(); ?>
 
+  <div class="content">
+    <div class="inner-content">
+
+        <?php
+        $args = array( 
+          'taxonomy' => 'jobs_cat',
+        );
+        $categories = get_terms( $args );
+        if ($categories) : ?>
+
+          <?php foreach ($categories as $cat) : ?>
+
+          <div class="block">
+            <div class="block-content">
+
+            <h2 class="job-category-title"><?php echo $cat->name; ?></h2>
+
+            <?php 
+            $args = array(
+              'post_type' => 'jobs',
+              'posts_per_page' => -1,
+              'tax_query' => array(
+                array(
+                  'taxonomy' => 'jobs_cat',
+                  'field'    => 'id',
+                  'terms' => $cat->term_id
+                )
+              ),
+            );
+            $the_query = new WP_Query( $args );
+            if ( $the_query->have_posts() ) : ?>
+
+              <div class="fs-wrap">
+
+              <?php
+              while ( $the_query->have_posts() ) : $the_query->the_post();
+                ?>
+
+                  <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+              <?php 
+              endwhile; ?>
+
+              </div>
+
+            <?php 
+            endif; //end if cats have posts?>
+
+          </div>
+        </div>
+
+            <?php
+          endforeach; //end foreach cat
+        endif; //end if categories 
+        wp_reset_postdata();
+        ?>
+
+
+    </div>
+  </div>
+
+
 <?php get_footer(); ?>
